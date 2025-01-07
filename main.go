3e2e4b975b91b81dda4cb84768aa771b4a9e62e6
Dawalhasa <dawalhasa@gmail.com>
2025-01-07 22:09:08 +0530

    How to use the variable in general in go language

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 
 func main(){
+	var conferenceName = "Go Conference"
 
-	fmt.Println("Welcome to our conference booking application!")
+	fmt.Println("Welcome to", conferenceName, "booking application!")
 	fmt.Println("Get you ticket to attend")
 
+	
 }
