f23d3f6d69ea70e4aca3b44f3c481744ed53c0d4
Dawalhasa <dawalhasa@gmail.com>
2025-01-07 22:33:00 +0530

    general variable useage

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import "fmt"
 
 func main(){
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50 
 
 	fmt.Println("Welcome to", conferenceName, "booking application!")
-	fmt.Println("Get you ticket to attend")
+	fmt.Println("We have total of", conferenceTickets, "and remaing", remainingTickets, "are still avialable")
+	fmt.Println("Get you ticket to attend the conference")
 
 	
 }
