299a639781785a3f4a6058eecf1fb6dbce418ca3
Dawalhasa <dawalhasa@gmail.com>
2025-01-07 23:17:08 +0530

    How to assign and declare variable datatype in golang

--- main.go
+++ main.go
@@ -4,13 +4,25 @@ import "fmt"
 
 
 func main(){
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50 
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50 
+
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still avialable\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get you ticket to attend the conference")
+	fmt.Println("Get your ticket to attend the conference")
 
+	var userName string
+	var userTickets int
+
+
+
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Println(userName, userTickets)
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 	
 }
